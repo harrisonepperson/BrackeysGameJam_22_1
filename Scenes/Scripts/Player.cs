eb5c79decdd6380defd1ba1b92bed2fa3d8cc475
Jay Harris <jharris812@gmail.com>
2022-02-20 17:57:24 -0500

    First steps adding a basic character.

--- Scenes/Scripts/Player.cs
+++ Scenes/Scripts/Player.cs
@@ -5,23 +5,42 @@ public class Player : KinematicBody
 {
 	[Export]
 	private int StepsToHideDirectionHint = 3;
-	
+
 	private RayCast wallCheck;
 	private int stepScale = 2;
-	
+
+	private AnimationPlayer animationPlayer;
+	private static string IdleAnimationName = "IdleLoop";
+	private static string WalkAnimationName = "WalkLoop";
+
 	public override void _Ready()
 	{
 		wallCheck = GetNode<RayCast>("Wall_Checker");
+
+
+		//Set up player animation
+		var animationPlayerPath = "character/AnimationPlayer";
+		animationPlayer = GetNode<AnimationPlayer>(animationPlayerPath);
+		if (animationPlayer == null)
+		{
+			throw new Exception("Animation Player not found at path: " + animationPlayerPath);
+		}
+		var idle = animationPlayer.GetAnimation(IdleAnimationName);
+		idle.Loop = true;
+		idle.TrackSetInterpolationType(0, Animation.InterpolationType.Nearest);
+		var walk = animationPlayer.GetAnimation(WalkAnimationName);
+		walk.TrackSetInterpolationType(0, Animation.InterpolationType.Nearest);
 	}
-	
+
+
 	public override void _Process(float delta)
 	{
-		if(StepsToHideDirectionHint == 0)
+		if (StepsToHideDirectionHint == 0)
 		{
 			GetNode<Spatial>("Direction_Hint").Visible = false;
 		}
-		
-		if(
+
+		if (
 			Input.IsActionJustPressed("move_forward") ||
 			Input.IsActionJustPressed("move_backward") ||
 			Input.IsActionJustPressed("move_left") ||
@@ -30,31 +49,44 @@ public class Player : KinematicBody
 		{
 			Vector3 pos = Translation;
 			Vector3 dir = new Vector3(0, 0, 0);
-		
-			if(Input.IsActionJustPressed("move_forward"))
+
+			if (Input.IsActionJustPressed("move_forward"))
 			{
 				dir.z -= stepScale;
-			} else if(Input.IsActionJustPressed("move_backward"))
+				animationPlayer.Play(WalkAnimationName);
+				animationPlayer.Queue(IdleAnimationName);
+			}
+			else if (Input.IsActionJustPressed("move_backward"))
 			{
 				dir.z += stepScale;
-			} else if(Input.IsActionJustPressed("move_left"))
+				animationPlayer.PlayBackwards(WalkAnimationName);
+				animationPlayer.Queue(IdleAnimationName);
+			}
+			else if (Input.IsActionJustPressed("move_left"))
 			{
 				dir.x -= stepScale;
-			} else if(Input.IsActionJustPressed("move_right"))
+				animationPlayer.PlayBackwards(WalkAnimationName);
+				animationPlayer.Queue(IdleAnimationName);
+			}
+			else if (Input.IsActionJustPressed("move_right"))
 			{
 				dir.x += stepScale;
+				animationPlayer.PlayBackwards(WalkAnimationName);
+				animationPlayer.Queue(IdleAnimationName);
 			}
-			
+
 			wallCheck.CastTo = dir;
 			wallCheck.ForceRaycastUpdate();
-			
+
 			if (wallCheck.IsColliding())
 			{
-			} else {
-				pos += dir;
-				StepsToHideDirectionHint --;
 			}
-			
+			else
+			{
+				pos += dir;
+				StepsToHideDirectionHint--;
+			}
+
 			Translation = pos;
 		}
 	}
