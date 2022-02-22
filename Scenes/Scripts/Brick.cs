41182bd5a4f7524b3b715fd7d9549937f26269f1
Jay Harris <jharris812@gmail.com>
2022-02-21 21:39:56 -0500

    Switching to AudioStreamPlayer from AudioStreamPlayer2D

--- Scenes/Scripts/Brick.cs
+++ Scenes/Scripts/Brick.cs
@@ -3,77 +3,77 @@ using System;
 
 public class Brick : Area
 {
-	[Export]
-	private float rotationSpeed = 5F;
+    [Export]
+    private float rotationSpeed = 5F;
 
-	private MeshInstance view;
-	private bool isMouseOn = false;
-	private bool isRotating = false;
-	private bool canClick = true;
-	private float targetDegrees;
+    private MeshInstance view;
+    private bool isMouseOn = false;
+    private bool isRotating = false;
+    private bool canClick = true;
+    private float targetDegrees;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		view = GetNode<MeshInstance>("View_Target");
-		targetDegrees = RotationDegrees.y;
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        view = GetNode<MeshInstance>("View_Target");
+        targetDegrees = RotationDegrees.y;
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(float delta)
-	{
-		if (isRotating)
-		{
-			if (Mathf.IsEqualApprox(RotationDegrees.y, targetDegrees, 30F))
-			{
-				canClick = true;
-			}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        if (isRotating)
+        {
+            if (Mathf.IsEqualApprox(RotationDegrees.y, targetDegrees, 30F))
+            {
+                canClick = true;
+            }
 
-			if (!Mathf.IsEqualApprox(RotationDegrees.y, targetDegrees, 1F))
-			{
-				Vector3 rot = RotationDegrees;
-				rot.y = targetDegrees;
+            if (!Mathf.IsEqualApprox(RotationDegrees.y, targetDegrees, 1F))
+            {
+                Vector3 rot = RotationDegrees;
+                rot.y = targetDegrees;
 
-				RotationDegrees = RotationDegrees.LinearInterpolate(rot, rotationSpeed * delta);
-			}
-			else
-			{
-				Vector3 rot = RotationDegrees;
-				rot.y = Mathf.Stepify(rot.y, 90F);
-				RotationDegrees = rot;
-				isRotating = false;
-			}
-		}
+                RotationDegrees = RotationDegrees.LinearInterpolate(rot, rotationSpeed * delta);
+            }
+            else
+            {
+                Vector3 rot = RotationDegrees;
+                rot.y = Mathf.Stepify(rot.y, 90F);
+                RotationDegrees = rot;
+                isRotating = false;
+            }
+        }
 
-		if (isMouseOn && canClick)
-		{
-			var blockRotate = GetNode<AudioStreamPlayer2D>("Sounds/block_rotate");
-			if (Input.IsActionJustPressed("Primary_Click"))
-			{
-				isRotating = true;
-				canClick = false;
-				targetDegrees = Mathf.Stepify(RotationDegrees.y + 90, 90);
-				blockRotate.Play();
-			}
-			else if (Input.IsActionJustPressed("Secondary_Click"))
-			{
-				isRotating = true;
-				canClick = false;
-				targetDegrees = Mathf.Stepify(RotationDegrees.y - 90, 90);
-				blockRotate.Play();
-			}
-		}
-	}
+        if (isMouseOn && canClick)
+        {
+            var blockRotate = GetNode<AudioStreamPlayer>("Sounds/block_rotate");
+            if (Input.IsActionJustPressed("Primary_Click"))
+            {
+                isRotating = true;
+                canClick = false;
+                targetDegrees = Mathf.Stepify(RotationDegrees.y + 90, 90);
+                blockRotate.Play();
+            }
+            else if (Input.IsActionJustPressed("Secondary_Click"))
+            {
+                isRotating = true;
+                canClick = false;
+                targetDegrees = Mathf.Stepify(RotationDegrees.y - 90, 90);
+                blockRotate.Play();
+            }
+        }
+    }
 
-	private void _on_Tunnel_Brick_mouse_entered()
-	{
-		view.Visible = true;
-		isMouseOn = true;
-	}
+    private void _on_Tunnel_Brick_mouse_entered()
+    {
+        view.Visible = true;
+        isMouseOn = true;
+    }
 
-	private void _on_Tunnel_Brick_mouse_exited()
-	{
-		view.Visible = false;
-		isMouseOn = false;
-	}
+    private void _on_Tunnel_Brick_mouse_exited()
+    {
+        view.Visible = false;
+        isMouseOn = false;
+    }
 }
