817d570b84954529af1c9ffd215da7328cf906a8
Jay Harris <jharris812@gmail.com>
2022-02-22 22:09:53 -0500

    Fixing menu bug.

--- Scenes/Menu/Scripts/OutroBG.cs
+++ Scenes/Menu/Scripts/OutroBG.cs
@@ -4,29 +4,29 @@ using System;
 public class OutroBG : Panel
 {
 
-    [Export]
-    private PackedScene MenuScene;
-    private ulong time_start = 0;
-    public override void _Ready()
-    {
-        var newColor = this.Modulate;
-        newColor.a = 0;
-        this.Modulate = newColor;
-        time_start = OS.GetUnixTime();
-    }
+	[Export]
+	private PackedScene MenuScene;
+	private ulong time_start = 0;
+	public override void _Ready()
+	{
+		var newColor = this.Modulate;
+		newColor.a = 0;
+		this.Modulate = newColor;
+		time_start = OS.GetUnixTime();
+	}
 
-    public override void _Process(float delta)
-    {
-        var newColor = this.Modulate;
-        newColor.a = Mathf.Min(1, newColor.a + (delta / 10F));
-        this.Modulate = newColor;
+	public override void _Process(float delta)
+	{
+		var newColor = this.Modulate;
+		newColor.a = Mathf.Min(1, newColor.a + (delta / 10F));
+		this.Modulate = newColor;
 
 
-        var time_elapsed = OS.GetUnixTime() - time_start;
+		var time_elapsed = OS.GetUnixTime() - time_start;
 
-        if (time_elapsed > 10)
-        {
-            GetTree().ChangeSceneTo(MenuScene);
-        }
-    }
+		if (time_elapsed > 10)
+		{
+			GetTree().ChangeSceneTo(MenuScene);
+		}
+	}
 }
