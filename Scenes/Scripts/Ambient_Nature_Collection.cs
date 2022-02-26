7b3466f6e9dd662648bdd927da426ced214a9240
Harrison <me@harrisonepperson.com>
2022-02-26 14:36:13 -0500

    Add sick-ass buttterflies

--- Scenes/Scripts/Ambient_Nature_Collection.cs
+++ Scenes/Scripts/Ambient_Nature_Collection.cs
@@ -16,14 +16,19 @@ public class Ambient_Nature_Collection : Spatial
 	private bool showFlowers = true;
 	
 	[Export]
+	private bool showButterflies = true;
+	
+	[Export]
 	private bool showVines = true;
 
 	public override void _Ready()
 	{
-		GetNode<Spatial>("Floater/Tree").Visible = showTree;
-		GetNode<Particles>("Floater/Tree/Falling Leaves").Emitting = showFallingLeaves;
+		GetNode<RigidBody>("Floater/Tree").Visible = showTree;
+		GetNode<CollisionShape>("Floater/Tree/Collider").Disabled = !showTree;
+		GetNode<Particles>("Floater/Tree/Tree/Falling Leaves").Emitting = showFallingLeaves;
 		GetNode<Spatial>("Floater/GrassTop").Visible = showGrassTop;
 		GetNode<Particles>("Floater/Flower_Patch").Emitting = showFlowers;
 		GetNode<Spatial>("Floater/Vines").Visible = showVines;
+		GetNode<Particles>("Floater/Butterflies/Flap").Emitting = showButterflies;
 	}
 }
