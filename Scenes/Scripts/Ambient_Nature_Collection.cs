0e25e4d92377a37d2603f5636c12adad2e3a8ef4
Harrison <me@harrisonepperson.com>
2022-02-26 15:32:55 -0500

    Rotate nature blocks randomly

--- Scenes/Scripts/Ambient_Nature_Collection.cs
+++ Scenes/Scripts/Ambient_Nature_Collection.cs
@@ -20,15 +20,21 @@ public class Ambient_Nature_Collection : Spatial
 	
 	[Export]
 	private bool showVines = false;
+	
+	private Random rand = new Random();
 
 	public override void _Ready()
 	{
+		Vector3 blockRot = new Vector3(0, rand.Next(0, 360), 0);
+		
 		GetNode<RigidBody>("Floater/Tree").Visible = showTree;
 		GetNode<CollisionShape>("Floater/Tree/Collider").Disabled = !showTree;
 		GetNode<Particles>("Floater/Tree/Tree/Falling Leaves").Emitting = showTree && showFallingLeaves;
-		GetNode<Spatial>("Floater/GrassTop").Visible = showGrassTop;
+		GetNode<Spatial>("Floater/Rock/GrassTop").Visible = showGrassTop;
 		GetNode<Particles>("Floater/Flower_Patch").Emitting = showFlowers;
 		GetNode<Spatial>("Floater/Vines").Visible = showVines;
 		GetNode<Particles>("Floater/Butterflies/Flap").Emitting = showButterflies;
+		
+		GetNode<MeshInstance>("Floater/Rock").Rotation = blockRot;
 	}
 }
